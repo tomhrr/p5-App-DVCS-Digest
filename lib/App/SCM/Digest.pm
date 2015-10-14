0c86b34c855458efc28002b97369129b4c7c8de6
Tom Harrison <tomh5908@gmail.com>
2015-10-14 18:40:57 +1000

    [master] fix from and to defaults

--- lib/App/SCM/Digest.pm
+++ lib/App/SCM/Digest.pm
@@ -183,9 +183,9 @@ sub _process_bounds
         $from = _strftime($time - 86400);
         $to   = _strftime($time);
     } elsif (not defined $from) {
-        $from = _strftime(0);
+        $from = '0000-01-01T00:00:00';
     } elsif (not defined $to) {
-        $to   = _strftime($time);
+        $to   = '9999-12-31T23:59:59';
     }
 
     my $config = $self->{'config'};
